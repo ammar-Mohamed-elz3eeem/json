bbe81a47825e5689d3a0894f66abb7195377e0f8
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-09-19 03:41:07 +0300

    Add testcase for json with null value

--- test/src/JSONTests.cpp
+++ test/src/JSONTests.cpp
@@ -13,6 +13,17 @@
 #include <gtest/gtest.h>
 #include <JSON/JSON.hpp>
 
-TEST(JSONTests, Placeholder)
+TEST(JSONTests, FromNull)
 {
+	JSON::JSON json(nullptr);
+	ASSERT_EQ("null", json.ToString());
+}
+
+TEST(JSONTests, ToNull)
+{
+	const auto json = JSON::JSON::FromString("null");
+	ASSERT_FALSE((bool)json);
+	ASSERT_TRUE(json == nullptr);
+}
+
 }
